afe916f032a46c14c326668084fcba02157116e4
RodolfoMRibeiro <89111970+RodolfoMRibeiro@users.noreply.github.com>
2022-08-19 19:41:52 -0300

    feature: create models

--- db/model/tables.go
+++ db/model/tables.go
@@ -1,6 +1,43 @@
 package model
 
-type Metas struct {
+type Goal struct {
 	Id   string `gorm:"id; primaryKey"`
 	Name string
+}
+
+type User struct {
+	Id       int  `gorm:"id; primaryKey"`
+	Goal     Goal `gorm:"foreignKey:Id"`
+	Email    string
+	Password string
+	Login    string
+	Fullname string
+}
+
+type Course struct {
+	Id            int `gorm:"id; primaryKey"`
+	Name          string
+	Description   string
+	ModulesAmount int
+}
+
+type CourseInProgress struct {
+	Courses []Course
+	Users   []User
+}
+
+type Module struct {
+	Id          int    `gorm:"id; primaryKey"`
+	CourseId    Course `gorm:"foreignKey:Id"`
+	CardsAmount int
+	Name        string
+	Description string
+}
+
+type Card struct {
+	Id       int    `gorm:"id; primaryKey"`
+	ModuleId Module `gorm:"foreignKey:Id"`
+	Name     string
+	Content  string
+	Kind     string
 }
